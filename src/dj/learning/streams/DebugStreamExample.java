b55d97ee40eff30ab03ff60bbbaf4c8e42dbccae
dibyajyoti_behera <dibyajyoti_behera@dellteam.com>
2021-02-04 12:25:26 +0530

    section 11 completed

---
+++ src/dj/learning/streams/DebugStreamExample.java
@@ -0,0 +1,20 @@
+package dj.learning.streams;
+
+import dj.learning.data.Student;
+import dj.learning.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DebugStreamExample {
+    public static void main(String[] args) {
+        Map<String, List<String>> studentMap = StudentDataBase.getAllStudents().stream()
+                .peek(System.out::println)  //debugging
+                .filter(student -> student.getGpa() >= 3.9)
+                .peek(System.out::println)  //debugging
+                .filter(student -> student.getGradeLevel() >= 3)
+                .peek(System.out::println)  //debugging
+                .collect(Collectors.toMap(Student::getName, Student::getActivities));  //desired result
+    }
+}
---
+++ src/dj/learning/streams/StreamsExample.java
@@ -0,0 +1,18 @@
+package dj.learning.streams;
+
+import dj.learning.data.Student;
+import dj.learning.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamsExample {
+    public static void main(String[] args) {
+        Map<String, List<String>> studentMap = StudentDataBase.getAllStudents().stream()
+                .filter(student -> student.getGpa() >= 3.9)  //stream of students
+                .filter(student -> student.getGradeLevel() >= 3) //stream of students
+                .collect(Collectors.toMap(Student::getName, Student::getActivities));  //desired result
+        System.out.println(studentMap);
+    }
+}
