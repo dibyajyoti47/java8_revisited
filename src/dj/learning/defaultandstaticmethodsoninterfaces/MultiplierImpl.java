ef7b359ad4575d22de32b7030d93b06c803f0699
dibyajyoti_behera <dibyajyoti_behera@dellteam.com>
2021-03-04 00:19:49 +0530

    section 18 compltd

---
+++ src/dj/learning/defaultandstaticmethodsoninterfaces/MultiplierImpl.java
@@ -0,0 +1,11 @@
+package dj.learning.defaultandstaticmethodsoninterfaces;
+
+import java.util.List;
+
+public class MultiplierImpl implements  Multiplier{
+
+    @Override
+    public int multiply (List<Integer> integerList) {
+        return integerList.stream().reduce(1, (x,y) -> x*y);
+    }
+}
